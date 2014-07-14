69deed1ec9e1365270aeef63fbb4e0de833e9c40
icantrap <icantrap08@gmail.com>
2014-07-14 00:00:31 -0400

    insecure profile for dev usage

---
+++ src/main/java/com/icantrap/examples/security/InsecureFilterChain.java
@@ -0,0 +1,16 @@
+package com.icantrap.examples.security;
+
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+
+public class InsecureFilterChain extends GenericFilterBean {
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+    filterChain.doFilter(request, response);
+  }
+}
